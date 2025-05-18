91efe2a0fa3b2d548f5bad5a1a793642f59ee399
Shubham Maurya <shubhammr21@gmail.com>
2025-05-18 22:31:12 +0530

    feat: Enhance data loading in routes by adding loader dependencies and queries

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,17 @@
 import DataDashboard from "@/components/charts/page"
+import {
+  getFetchDashboardStatsQuery,
+  getFetchFacetsQuery
+} from "@/services/reports/queries"
 import type { RecordFilterParams } from "@/services/reports/types"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
   component: DataDashboard,
-  validateSearch: () => ({}) as RecordFilterParams
+  validateSearch: search => search as RecordFilterParams,
+  loaderDeps: ({ search }) => search,
+  loader: ({ context, deps }) => {
+    context.queryClient.ensureQueryData(getFetchDashboardStatsQuery(deps))
+    context.queryClient.ensureQueryData(getFetchFacetsQuery())
+  }
 })
--- frontend/src/routes/table.tsx
+++ frontend/src/routes/table.tsx
@@ -1,8 +1,17 @@
 import DataRecordTablePage from "@/components/records/page"
+import {
+  getFetchDashboardTableQuery,
+  getFetchFacetsQuery
+} from "@/services/reports/queries"
 import type { PaginatedRecordFilterParams } from "@/services/reports/types"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/table")({
   component: DataRecordTablePage,
-  validateSearch: () => ({}) as PaginatedRecordFilterParams
+  validateSearch: () => ({}) as PaginatedRecordFilterParams,
+  loaderDeps: ({ search }) => search,
+  loader: ({ context, deps }) => {
+    context.queryClient.ensureQueryData(getFetchDashboardTableQuery(deps))
+    context.queryClient.ensureQueryData(getFetchFacetsQuery())
+  }
 })
